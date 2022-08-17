debd0bebfa86b9c0c7c4d4c213e3c4853cc9d212
mochammadiqbal1 <111103517+mochammadiqbal1@users.noreply.github.com>
2022-08-17 11:29:21 +0700

    Update powershellscripts.ps1

--- Coding-PGT/project-1/kemampuan-dasar-1/latihan/shellscripts/powershellscripts.ps1
+++ Coding-PGT/project-1/kemampuan-dasar-1/latihan/shellscripts/powershellscripts.ps1
@@ -1,1 +1,12 @@
 Write-Host "Saya"
+Get-Content
+Get-Date
+Set-ExecutionPolicy, AllSigned
+Test-Connection, AlamatIp
+New-File / Ni
+Get-help
+Add-Content
+Test-NetConnection
+Set-Content
+Get-Command / gcm
+Get-Clipboard
